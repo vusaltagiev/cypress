7da58e10746995bde56f42b8ea31bc34b007585b
Vüsal Tagiev <88597801+vusaltagiev@users.noreply.github.com>
2026-01-13 18:40:07 +0100

    install de XPath Dependency

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,3 +1,4 @@
+require("@cypress/xpath");
 // ***********************************************************
 // This example support/e2e.ts is processed and
 // loaded automatically before your test files.
@@ -14,4 +15,4 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import "./commands";

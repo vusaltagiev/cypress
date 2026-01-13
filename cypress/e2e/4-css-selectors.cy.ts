51960b9d56ebc49c14d57aa43cce4124d1f07452
Vüsal Tagiev <88597801+vusaltagiev@users.noreply.github.com>
2026-01-13 18:35:10 +0100

    how to find a class

--- cypress/e2e/4-css-selectors.cy.ts
+++ cypress/e2e/4-css-selectors.cy.ts
@@ -5,4 +5,17 @@ describe("Locators", () => {
   it("Cy.Contains ex", () => {
     cy.contains("Mer information").should("contain", "Mer information");
   });
+
+  it("Cy.get + cy.find ex", () => {
+    cy.get("div").find("button").should("contain", "Mer information");
+  });
+  it("Css selector using an attribute", () => {
+    cy.get('button[class="btn btn-primary py-2 px-4"]').should(
+      "contain",
+      "Mer information"
+    );
+  });
+  it("Css selector using a class", () => {
+    cy.get(".btn-primary.py-2.px-4").should("contain", "Mer information");
+  });
 });

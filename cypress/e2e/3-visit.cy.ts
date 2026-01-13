73ec59c502f762844334181db0ca6b2ac45cca0f
Vüsal Tagiev <88597801+vusaltagiev@users.noreply.github.com>
2026-01-13 14:39:30 +0100

    added get

--- cypress/e2e/3-visit.cy.ts
+++ cypress/e2e/3-visit.cy.ts
@@ -1,14 +1,22 @@
 describe("basics", () => {
   beforeEach(() => {
-    cy.visit("textinput");
-  });
-  it("visit", () => {
-    cy.visit("textinput");
+    cy.visit("/diseases-symptoms/allergies");
   });
   it("visit", () => {
     cy.url().then((url) => {
       cy.log(`print url: ${url}`);
-      expect(url).to.contains("textinput");
+      expect(url).to.contains("/diseases-symptoms/allergies");
     });
+  });
+  it("title validation", () => {
+    cy.title().then((title) => {
+      cy.log(title);
+      expect(title).to.be.eq(
+        "Din Sjuksköterska - Videoguider om sjukdomar och symtom | Legitimerad sjuksköterska"
+      );
+    });
+  });
+  it("span challenge", () => {
+    cy.get("span.me-2 ");
   });
 });

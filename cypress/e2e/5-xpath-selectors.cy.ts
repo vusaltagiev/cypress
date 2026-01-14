9cb7b98d9508d54db8d499cf0cc7c3b7418b1349
Vüsal Tagiev <88597801+vusaltagiev@users.noreply.github.com>
2026-01-14 11:26:31 +0100

    find by class

--- cypress/e2e/5-xpath-selectors.cy.ts
+++ cypress/e2e/5-xpath-selectors.cy.ts
@@ -3,6 +3,16 @@ describe("Locators", () => {
     cy.visit("/diseases-symptoms/allergies");
   });
   it("how to find a element by its text", () => {
-    cy.xpath();
+    cy.xpath(`//*[text()='Allergier']`).should("contain.text", "Allergier");
+  });
+  it("find an element by its attribute using xpath", () => {
+    cy.xpath(`//h1[@class='mb-3']`).should("contain.text", "Videoklipp");
+  });
+  it("find by class", () => {
+    cy.xpath(`//button[@class='btn btn-primary py-2 px-4']`).should(
+      "have.css",
+      "background-color",
+      "rgb(0, 102, 188)"
+    );
   });
 });

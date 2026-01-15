781e185a0a138922cac8172732e98302ff9bee8e
Vüsal Tagiev <88597801+vusaltagiev@users.noreply.github.com>
2026-01-15 15:11:35 +0100

    Dynamic table

--- cypress/e2e/7-ui-scenariosl.cy.ts
+++ cypress/e2e/7-ui-scenariosl.cy.ts
@@ -9,7 +9,7 @@ describe.skip("Click challenge", () => {
   });
 });
 
-describe("Hover challenge", () => {
+describe.skip("Hover challenge", () => {
   beforeEach(() => {
     cy.visit("/diseases-symptoms/allergies");
   });
@@ -18,5 +18,45 @@ describe("Hover challenge", () => {
   });
   it("hover with real hover elements", () => {
     cy.get(".nav > :nth-child(2) > .btn").realHover;
+  });
+});
+
+describe.skip("Hover challenge", () => {
+  beforeEach(() => {
+    cy.visit("/diseases-symptoms/allergies");
+  });
+  it("hover with real hover elements", () => {
+    cy.get(".nav > :nth-child(2) > .btn").trigger("mouseover");
+  });
+  it("hover with real hover elements", () => {
+    cy.get(".nav > :nth-child(2) > .btn").realHover;
+  });
+});
+
+describe("Dynamic table challenge", () => {
+  beforeEach(() => {
+    cy.visit("/diseases-symptoms/allergies");
+  });
+  it("Chrome CPU Test", () => {
+    cy.get(":nth-child(1) > .property-item > .p-4").each(($button) => {
+      if ($button.text().includes("Mer information")) {
+        cy.log(`I have found the ${$button.text()} row!`);
+        let chromeRowValues: string[] = [];
+        chromeRowValues.push($button.next().text());
+        chromeRowValues.push($button.next().next().text());
+        chromeRowValues.push($button.next().next().next().text());
+        chromeRowValues.push($button.next().next().next().next().text());
+        cy.log("Chrome row values", chromeRowValues);
+        chromeRowValues.forEach((chromeValue) => {
+          if (chromeValue.includes("%")) {
+            cy.log(chromeValue);
+            cy.get(".bg-warning").should(
+              "have.text",
+              `Mer information: ${chromeValue}`
+            );
+          }
+        });
+      }
+    });
   });
 });

60b1472859de1d8bb5eb7677b314d74a557774f8
Vüsal Tagiev <88597801+vusaltagiev@users.noreply.github.com>
2026-01-17 13:39:02 +0100

    WIP

---
+++ cypress/e2e/9-basic-viewport-configuration.cy.ts
@@ -0,0 +1,7 @@
+describe("cy.viewport() demo", () => {
+  beforeEach(() => {
+    cy.visit(`${Cypress.env("dinSSK")}//diseases-symptoms/allergies`);
+  });
+  it("device name", () => {});
+  it("specific viewport", () => {});
+});

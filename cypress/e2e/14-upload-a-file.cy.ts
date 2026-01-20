4802ef03b29b152c40c1c0d139faee81125128aa
Vüsal Tagiev <88597801+vusaltagiev@users.noreply.github.com>
2026-01-20 21:12:57 +0100

    WIP

---
+++ cypress/e2e/14-upload-a-file.cy.ts
@@ -0,0 +1,10 @@
+describe("Upload a file in the QA Demo site", () => {
+  beforeEach(() => {
+    cy.visit(`${Cypress.env("demoQA")}/upload-download`);
+  });
+  it("Upload the file", () => {
+    cy.get("input#uploadFile").attachFile("example.json");
+    cy.get("p#uploadedFilePath").should("contain", "example.json");
+  });
+});
+// filen går att lägg in i mappen fixtures
